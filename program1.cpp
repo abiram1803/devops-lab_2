c7e778db560345754da61dcd88d00191891a90c0
abiram1803 <rshreeabiram@gmail.com>
2026-07-21 18:51:08 +0530

    Initial Commit

---
+++ program1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Declare variables to store the numbers and the result
+    double num1, num2, product;
+
+    //prompt the user for input
+    cout<<"Enter first number:";
+    cin>>num1;
+
+    cout<<"Enter the second number:";
+    cin>>num2;
+
+    //perform the mutliplication
+    product = num1*num2;
+
+    cout<<"The product of "<<num1<<"and"<<num2<<"is:"<<product<<endl;
+    return 0;
+}
